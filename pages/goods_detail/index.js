6a8ebb637895b95ac9451709754b5d8522d83364
chenmingyu <cdy1571@163.com>
2020-11-21 16:12:09 +0800

    新增底部工具栏，商品详情页，利用本地缓存实现加入购物车功能

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,3 +1,4 @@
+import { request } from "../../request/index";
 // pages/goods_detail/index.js
 Page({
 
@@ -5,62 +6,73 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    goodsObj: {}
   },
+
+  GoodsInfo: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const { goods_id } = options
+    console.log(goods_id);
+    this.getDetail(goods_id)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取商品详情数据
+  async getDetail(id) {
+    let param = {
+      url: "/goods/detail",
+      data: { goods_id: id }
+    }
+    const goodsObj = await request(param);
+    this.GoodsInfo = goodsObj
+    console.log(this.GoodsInfo);
+    this.setData({
+      goodsObj: {
+        goods_name: goodsObj.goods_name,
+        goods_price: goodsObj.goods_price,
+        // iphone部分手机 不识别webp图片格式
+        // 1.找后台修改图片格式
+        // 2.临时自己修改 ，确保后台存在 jpg格式的图片 ,前端只需将webp替换成jpg（字符串）
+        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: goodsObj.pics
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 轮播图放大预览图  调用小程序官方的api previewImage
+  handlePreviewImage(e) {
+    console.log('e', e.currentTarget.dataset.url);
+    const urls = this.GoodsInfo.pics.map(v => v.pics_mid)
+    const current = e.currentTarget.dataset.url
+    wx.previewImage({
+      current, // 当前显示图片的http链接
+      urls // 需要预览的图片http链接列表
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 加入购物车
+  handleCartAdd() {
+    console.log('购物车');
+    // 1.获取缓存中的购物车数据 数组
+    let cart = wx.getStorageSync("cart") || [];
+    // 2. 判断商品对象是否存在于购物车数组中
+    let index = cart.findIndex(v => v.goods_id == this.GoodsInfo.goods_id);
+    if (index === -1) {
+      // 不存在 第一次添加
+      this.GoodsInfo.num = 1;
+      cart.push(this.GoodsInfo)
+    } else {
+      // 已经存在购物车数据 执行 num++
+      cart[index].num++
+    }
+    wx.setStorageSync("cart", cart);
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      mask: true,
+    });
   }
 })
