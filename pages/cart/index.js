c7d85aa4feded87deb8b1efe325cb33cd32b8eac
chenmingyu <cdy1571@163.com>
2020-11-30 21:46:14 +0800

    新增获取地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,48 @@
 // pages/cart/index.js
-Page({
+// 1. 获取用户到收货地址
+//    1. 给按钮绑定点击事件
+//    2. 调用小程序官方的api 获取用户的收货地址
+import { chooseAddress } from "../../utils/asyncWx";
+import regeneratorRuntime from "../../lib/runtime/runtime";
 
+Page({
   /**
    * 页面的初始数据
    */
-  data: {
-
-  },
+  data: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function (options) {},
 
+  // 获取收货地址  9.25后开发者可以直接调用wx.chooseAddress该接口，无需用户授权
+  // handleGetAddress() {
+  //   wx.getSetting({
+  //     success: (result) => {
+  //       const scopeAddress = result.authSetting["scope.address"];
+  //       if (scopeAddress === true || scopeAddress === undefined) {
+  //         wx.chooseAddress({
+  //           success: (result1) => {
+  //             console.log("result1", result1);
+  //           },
+  //         });
+  //       } else {
+  //         // 用户拒绝过授予权限，诱导用户打开授权页面
+  //         wx.openSetting({
+  //           success: (result2) => {
+  //             wx.chooseAddress({
+  //               success: (result3) => {
+  //                 console.log("result3", result3);
+  //               },
+  //             });
+  //           },
+  //         });
+  //       }
+  //     },
+  //   });
+  async handleGetAddress() {
+    const res = await chooseAddress();
+    console.log("res", res);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
---
+++ utils/asyncWx.js
@@ -0,0 +1,13 @@
+// 获取收货地址
+export const chooseAddress = () => {
+  return new Promise((resolve, reject) => {
+    wx.chooseAddress({
+      success: (result) => {
+        resolve(result);
+      },
+      fail: (err) => {
+        reject(err);
+      },
+    });
+  });
+};
