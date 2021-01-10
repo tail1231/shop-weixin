f8ebe77731216b05454d79e5deb43da32db96a59
chenmingyu <cdy1571@163.com>
2021-01-10 20:39:48 +0800

    完成购物车静态页，部分功能完善

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -2,6 +2,13 @@
 // 1. 获取用户到收货地址
 //    1. 给按钮绑定点击事件
 //    2. 调用小程序官方的api 获取用户的收货地址
+//    3. 把获取到的收货地址 存入到 本地存储中去
+//
+/**
+ * 2. 页面加载完完毕
+ *    1. 获取本地存储中到地址数据
+ *    2. 把数据设置给data中的一个变量
+ */
 import { chooseAddress } from "../../utils/asyncWx";
 import regeneratorRuntime from "../../lib/runtime/runtime";
 
@@ -9,7 +16,27 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {},
+  data: {
+    address: {}, // 收货地址
+    cart: [], // 购物车数据
+    allChecked: false, // 是否全选
+  },
+
+  onShow() {
+    // 1. 获取缓存中的收货地址信息
+    const address = wx.getStorageSync("address");
+    // 1. 获取缓存中的购物车数据
+    const cart = wx.getStorageSync("cart") || [];
+    // 1. 计算当前购物车商品是否都已被选中
+    // 假如空数组调用了every 方法，返回值就为true
+    const allChecked = cart.length > 0 && cart.every((v) => v.isCheck);
+    // 2. 给data赋值
+    this.setData({
+      address,
+      cart,
+      allChecked,
+    });
+  },
 
   /**
    * 生命周期函数--监听页面加载
@@ -42,7 +69,23 @@ Page({
   //     },
   //   });
   async handleGetAddress() {
-    const res = await chooseAddress();
-    console.log("res", res);
+    try {
+      const address = await chooseAddress();
+      address.allStr =
+        address.provinceName +
+        address.cityName +
+        address.countyName +
+        address.countyName +
+        address.detailInfo;
+      // 存入到缓存中去
+      wx.setStorageSync("address", address);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  // 全选购物车商品
+  handleCheckAll(e) {
+    console.log("object", e);
   },
 });
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,12 +1,11 @@
 import { request } from "../../request/index";
 // pages/goods_detail/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    goodsObj: {}
+    goodsObj: {},
   },
 
   GoodsInfo: {},
@@ -15,19 +14,19 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const { goods_id } = options
+    const { goods_id } = options;
     console.log(goods_id);
-    this.getDetail(goods_id)
+    this.getDetail(goods_id);
   },
 
   // 获取商品详情数据
   async getDetail(id) {
     let param = {
       url: "/goods/detail",
-      data: { goods_id: id }
-    }
+      data: { goods_id: id },
+    };
     const goodsObj = await request(param);
-    this.GoodsInfo = goodsObj
+    this.GoodsInfo = goodsObj;
     console.log(this.GoodsInfo);
     this.setData({
       goodsObj: {
@@ -36,43 +35,44 @@ Page({
         // iphone部分手机 不识别webp图片格式
         // 1.找后台修改图片格式
         // 2.临时自己修改 ，确保后台存在 jpg格式的图片 ,前端只需将webp替换成jpg（字符串）
-        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
-        pics: goodsObj.pics
-      }
-    })
+        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, ".jpg"),
+        pics: goodsObj.pics,
+      },
+    });
   },
 
   // 轮播图放大预览图  调用小程序官方的api previewImage
   handlePreviewImage(e) {
-    console.log('e', e.currentTarget.dataset.url);
-    const urls = this.GoodsInfo.pics.map(v => v.pics_mid)
-    const current = e.currentTarget.dataset.url
+    console.log("e", e.currentTarget.dataset.url);
+    const urls = this.GoodsInfo.pics.map((v) => v.pics_mid);
+    const current = e.currentTarget.dataset.url;
     wx.previewImage({
       current, // 当前显示图片的http链接
-      urls // 需要预览的图片http链接列表
-    })
+      urls, // 需要预览的图片http链接列表
+    });
   },
 
   // 加入购物车
   handleCartAdd() {
-    console.log('购物车');
+    console.log("购物车");
     // 1.获取缓存中的购物车数据 数组
     let cart = wx.getStorageSync("cart") || [];
     // 2. 判断商品对象是否存在于购物车数组中
-    let index = cart.findIndex(v => v.goods_id == this.GoodsInfo.goods_id);
+    let index = cart.findIndex((v) => v.goods_id == this.GoodsInfo.goods_id);
     if (index === -1) {
       // 不存在 第一次添加
       this.GoodsInfo.num = 1;
-      cart.push(this.GoodsInfo)
+      this.GoodsInfo.isCheck = true;
+      cart.push(this.GoodsInfo);
     } else {
       // 已经存在购物车数据 执行 num++
-      cart[index].num++
+      cart[index].num++;
     }
     wx.setStorageSync("cart", cart);
     wx.showToast({
-      title: '加入成功',
-      icon: 'success',
+      title: "加入成功",
+      icon: "success",
       mask: true,
     });
-  }
-})
+  },
+});
